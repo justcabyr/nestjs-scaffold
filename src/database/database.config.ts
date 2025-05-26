5e9ab6648915c80ed92c4f259f6780884c73aa3d
AbdulKabir Sulaiman <justcabyr@gmail.com>
2025-05-26 19:26:27 -0230

    feat: added databse module

---
+++ src/database/database.config.ts
@@ -0,0 +1,14 @@
+import { registerAs } from '@nestjs/config';
+
+export default registerAs('database', () => ({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  username: process.env.DB_USERNAME || 'postgres',
+  password: process.env.DB_PASSWORD || 'postgres',
+  database: process.env.DB_DATABASE || 'nestjs_scaffold',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+  synchronize: process.env.NODE_ENV !== 'production',
+  logging: process.env.NODE_ENV !== 'production',
+}));
