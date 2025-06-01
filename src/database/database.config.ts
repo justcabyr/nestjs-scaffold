c446721ee3726ae965fc149370dac95c1e300ed2
AbdulKabir Sulaiman <justcabyr@gmail.com>
2025-05-31 23:42:18 -0230

    chore: updated db config variables

--- src/database/database.config.ts
+++ src/database/database.config.ts
@@ -2,11 +2,11 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
   type: 'postgres',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT || 5432,
-  username: process.env.DATABASE_USERNAME || 'postgres',
-  password: process.env.DATABASE_PASSWORD || 'postgres',
-  database: process.env.DATABASE_NAME || 'scaffold',
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   migrations: ['dist/database/migrations/*{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
